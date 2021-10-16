357c4a11dedf18ff7adfebd30977bad20fe69b46
Yuki Tanaka <yuki_tanaka@commude.co.jp>
2021-10-16 17:20:34 +0900

    Hello Name

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Welcome name="Hagiwara" />
       <StatusBar style="auto" />
     </View>
   );
 }
 
+function Welcome(props) {
+  return <Text>Hello, {props.name}</Text>;
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  name: {
+    color: "red",
   },
 });
