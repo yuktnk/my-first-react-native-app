500a27b951479c2d490890010b2a80782aba82d4
Yuki Tanaka <yuki_tanaka@commude.co.jp>
2021-10-23 17:28:38 +0900

    Count Up

--- App.js
+++ App.js
@@ -1,17 +1,35 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { Welcome } from "./Welcome";
+import { StyleSheet, Text, View, Button } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Welcome name="Hagiwara" />
-      <Welcome name="Narisawa" />
-      <Welcome name="Tanaka" />
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  onPressMinusOne = () => {
+    this.setState({
+      count: this.state.count - 1,
+    });
+  };
+
+  onPressPlusOne = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button title="-1" onPress={this.onPressMinusOne} />
+        <Text>{this.state.count}</Text>
+        <Button title="+1" onPress={this.onPressPlusOne} />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
