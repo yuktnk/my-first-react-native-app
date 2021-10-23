9c8c3355f101df6f31709c884a2c42ad735d4e68
Yuki Tanaka <yuki_tanaka@commude.co.jp>
2021-10-23 18:52:44 +0900

    関数コンポーネント

--- App.js
+++ App.js
@@ -1,54 +1,24 @@
-import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import React, { useState } from "react";
+import { Text, View, Button, StyleSheet } from "react-native";
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      date: new Date(),
-    };
-  }
+export default function App() {
+  const [count, setCount] = useState(0);
 
-  componentDidMount() {
-    this.timerID = setInterval(() => {
-      this.tick();
-    }, 1000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState({
-      date: new Date(),
-    });
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.text}>現在時刻</Text>
-        <Text style={styles.text}>{this.state.date.toLocaleTimeString()}</Text>
-        <Text style={styles.text2}>がんばれ！！！</Text>
-      </View>
-    );
-  }
+  return (
+    <View style={styles.container}>
+      <Button title="+1" onPress={() => setCount(count + 1)} />
+      <Text>{count}</Text>
+      <Button title="-1" onPress={() => setCount(count - 1)} />
+      ttt ttt ttt ttt ttt ttt
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
+    backgroundColor: "#fff",
+    flex: 1,
     justifyContent: "center",
-  },
-  text: {
-    fontSize: 48,
-  },
-  text2: {
-    marginTop: 20,
-    fontSize: 48,
-    color: "red",
   },
 });
